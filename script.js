da415dbf4560ba9c4d8f3fbff971bbadb80ecf9e
vivek kumar <vk8584686@gmail.com>
2024-08-14 23:36:08 +0530

    update all file

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function toggleNavbar() {
-  const navBar = document.querySelector('.nav-bar');
-  navBar.classList.toggle('active');
+  const navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
 }
 
-document.querySelectorAll('.nav-link').forEach(link => {
-  link.addEventListener('click', () => {
-      const navBar = document.querySelector('.nav-bar');
-      navBar.classList.remove('active');
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const navBar = document.querySelector(".nav-bar");
+    navBar.classList.remove("active");
   });
 });
 
-var typed = new Typed('#element', {
-    strings: [' Wed Developer', 'Desinger', 'App Developer', 'Developer'],
-    typeSpeed: 170,
-    backSpeed: 80,
-    loop: true
+var typed = new Typed("#element", {
+  strings: [" Wed Developer", "Desinger", "App Developer", "Developer"],
+  typeSpeed: 170,
+  backSpeed: 80,
+  loop: true,
 });
